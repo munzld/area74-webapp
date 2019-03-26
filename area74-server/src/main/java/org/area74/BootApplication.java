4cf5cf5af48204da934df87d6468b9b6c18dc8b8
munnsl <munnsld9805@gmail.com>
2019-03-25 21:56:46 -0500

    - adding web security config basics  - updating boot application  - removing auth0 from package.json

--- area74-server/src/main/java/org/area74/BootApplication.java
+++ area74-server/src/main/java/org/area74/BootApplication.java
@@ -1,13 +1,13 @@
-package org.area74.web;
+package org.area74;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebApplication {
+public class BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
+		SpringApplication.run(BootApplication.class, args);
 	}
 
 }
