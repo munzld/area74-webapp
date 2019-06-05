821247b2e131ea02206efc201e11dfb008aecd3d
Lucas Munns <munnsld@Lucass-MacBook-Pro.local>
2019-06-04 21:33:39 -0500

    Material datatable on meetings page complete

--- src/app/meetings/meetings.module.ts
+++ src/app/meetings/meetings.module.ts
@@ -1,21 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MeetingsComponent } from './meetings.component';
 import { SharedModule } from '../shared/shared.module';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatInputModule } from '@angular/material';
+import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
   declarations: [MeetingsComponent],
-  imports: [
-    SharedModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatFormFieldModule,
-    MatTableModule,
-    MatInputModule
-  ]
+  imports: [SharedModule, MaterialModule]
 })
 export class MeetingsModule {}
