144878a6e41540b13a75908924b503ce41a8b8a6
Lucas Munns <munnsld@Lucass-MacBook-Pro.local>
2019-08-21 21:33:00 -0500

    - Bridging the Gap updates

--- src/app/bridging-the-gap/bridging-the-gap.module.ts
+++ src/app/bridging-the-gap/bridging-the-gap.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { DataTablesModule } from 'angular-datatables';
 import { BridgingTheGapComponent } from './bridging-the-gap.component';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [BridgingTheGapComponent],
-  imports: [SharedModule, DataTablesModule]
+  imports: [SharedModule, DataTablesModule, RouterModule]
 })
 export class BridgingTheGapModule {}
